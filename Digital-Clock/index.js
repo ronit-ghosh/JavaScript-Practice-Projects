c892fbc83469b58908c00c9e36591693308bab71
ronit-ghosh <ronitghosh06@gmail.com>
2024-04-26 20:21:18 +0530

    Files Added

---
+++ Digital-Clock/index.js
@@ -0,0 +1,28 @@
+function clock(){
+    const hour = document.querySelector(".hours");
+    const min = document.querySelector(".minutes");
+    const sec = document.querySelector(".seconds");
+
+    const now = new Date();
+    let hours = now.getHours();
+    let meridiem = hours>=12 ? "PM" : "AM";
+    hours = hours%12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    
+    hour.textContent = hours;
+    min.textContent = minutes;
+    sec.textContent = `${seconds} ${meridiem}`;
+
+    //Background Color Change as Time changes
+    if (hours>12){
+        document.body.style.backgroundColor = "rgb(241, 255, 240)"
+    }
+    else{
+        document.body.style.backgroundColor = "aliceblue"
+    }
+}
+
+clock();
+setInterval(clock, 1000);
